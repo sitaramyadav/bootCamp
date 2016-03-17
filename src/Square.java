d764b7b93ed7b541d9065100be8bfda0232772a0
VirajParab <parabveer01@gmail.com>
2016-03-17 13:22:15 +0530

    Extanded Rectangle by Square

--- src/Square.java
+++ src/Square.java
@@ -2,24 +2,16 @@
 Job of Square is to calculate any quantity of Square using length and breadth
     or length or breadth.
  */
-public class Square {
+public class Square extends Rectangle{
     private double side;
 
     private Square(double side) {
-        this.side = side;
+        super(side,side);
     }
 
     public static Square createSquare(double side) {
         if(side <= 0)
             throw new IllegalArgumentException("Non positive input is provided side: "+side+" while creating square");
         return new Square(side);
-    }
-
-    public double calculateArea() {
-        return this.side*this.side;
-    }
-
-    public double calculatePerimeter() {
-        return 4*this.side;
     }
 }
